5f311ece0651e39b7b5365cb9aa7c88d8412526e
nguyen hiep <nguyen hiep>
2016-09-18 10:15:31 +0700

    up

--- eictu/resources/views/welcome.blade.php
+++ eictu/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel EICTU
                 </div>
 
                 <div class="links">
