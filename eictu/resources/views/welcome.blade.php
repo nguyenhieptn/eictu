4b43df8d24e9a7f814c55546be260e8a51e9d756
dvkhang <khoaimonchien@gmail.com>
2016-09-18 16:03:18 +0700

    testedit

--- eictu/resources/views/welcome.blade.php
+++ eictu/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   hello ictu abc
+                   hello ictu abc-d
                 </div>
 
                 <div class="links">
