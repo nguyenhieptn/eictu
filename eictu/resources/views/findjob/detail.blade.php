d7760dda9cb166f9ec71eb6d494338207efd50f3
vagrant <vagrant@homestead>
2016-09-21 07:58:18 +0000

    Find Job

---
+++ eictu/resources/views/findjob/detail.blade.php
@@ -0,0 +1,28 @@
+@extends('findjob.layout.app')
+@section('content')
+	 <div class="container">
+     <div class="row">
+         <div class="col-md-12">
+             <div class="panel panel-default">
+                 <div class="panel-heading">eICTuStudentJobDetail - Bản tin tìm việc chi tiết của sinh viên</div>
+                 <div class="panel-body">
+                    <div class="entry-content">
+                        <h4>Nội dung bản tin của sinh viên:</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    </hr>
+                      <div class="entry-content">
+                        <h4>Thông tin Sinh viên</h4>
+                        <p>Họ và tên: <strong>Nguyen duc dai</strong></p>
+                        <p>Giới tính:<strong> _MAN_ </strong></p>
+                    </div>
+             </div>
+         </div>
+     </div>
+ </div>
+@endsection
--- eictu/routes/web.php
+++ eictu/routes/web.php
@@ -66,6 +66,27 @@ Route::group(['prefix' => 'iwant'], function(){
 	]);
 });
 
+/*FIND JOB*/
+Route::group(['prefix' => 'findjob'], function(){
+
+	Route::get('index', [
+		'as'=>'findjob.index',
+		'uses'=>'FindJob\FindJobControler@getIndex'
+
+	]);
+	Route::get('post', [
+		'as'=>'findjob.post',
+		'uses'=>'FindJob\FindJobControler@getPost'
+
+	]);
+
+	Route::get('detail', [
+		'as'=>'findjob.detail',
+		'uses'=>'FindJob\FindJobControler@getDetail'
+
+	]);
+});
+
 
 
 
