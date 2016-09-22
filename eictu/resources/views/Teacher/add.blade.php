999a805f2c0b45f1d1cb101ff1224e959c7a2507
dvkhang <khoaimonchien@gmail.com>
2016-09-22 21:43:24 +0700

    t

--- eictu/resources/views/Teacher/add.blade.php
+++ eictu/resources/views/Teacher/add.blade.php
@@ -8,7 +8,25 @@
                 <div class="panel-heading">Thêm giảng viên</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('teacher.add') }}">
-                        {{ csrf_field() }}
+                        <input type="hidden" name="_token" value="{{Session::token()}}">
+
+                        <div class="form-group">
+                            <label for="major" class="col-md-4 control-label">Ngành Học</label>
+
+                            <div class="col-md-6">
+                                <select name="major" class="form-control">
+                                    <option value="">Chọn ngành dạy</option>
+                                    @if(!$major)
+
+                                    @else
+                                        @foreach($major as $item_major)
+                                            <option value="{!! $item_major['id'] !!}">{!! $item_major['name'] !!}</option>
+
+                                        @endforeach
+                                    @endif    
+                                </select>
+                            </div>
+                        </div>
 
                         <div class="form-group{{ $errors->has('code') ? ' has-error' : '' }}">
                             <label for="code" class="col-md-4 control-label">Mã Giảng Viên</label>
