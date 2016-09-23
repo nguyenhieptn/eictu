6d780a9eb379ab992788410e734cc475484ae79b
bvgiap <bvgiap@gmail.com>
2016-09-23 08:04:10 +0700

    edit table students.coloum class_id : nullable

--- eictu/routes/web.php
+++ eictu/routes/web.php
@@ -20,17 +20,6 @@ Route::group(['prefix' => 'ktx'], function(){
 	Route::get('searchStudent', ['as'=> 'ktx.getSearch','uses' => 'QLKTX\SinhvienKtxController@getSearch']);
 });
 /*=== het Quan ly ky tuc xa======*/
-/*
-	Routes nhóm quản lý lớp h�?c
-*/
-Route::group(['prefix' => 'qllh'], function () {
-    Route::get('/', ['as' => 'qllh.trangchu', 'uses' => 'QLLopHoc@trangchu']);
-    Route::get('dssv/{idlop}', ['as' => 'qllh.dssvtronglop', 'uses' => 'QLLopHoc@dssv']);
-    Route::get('phanlop/{idlop}', ['as' => 'qllh.trangphanlop', 'uses' => 'QLLopHoc@trangphanlop']);
-    Route::get('phanlop/table/{idlop}', ['as' => 'qllh.trangphanlop', 'uses' => 'QLLopHoc@bangsvmoi']);
-    Route::post('phanlop/{idlop}', ['as' => 'qllh.phanlop', 'uses' => 'QLLopHoc@phanlop']);
-    Route::get('sinhnhat/{idlop}', ['as' => 'qllh.sinhnhat', 'uses' => 'QLLopHoc@sinhnhatbancunglop']);
-});
 
 Auth::routes();
 
