898ef0ab606cc25b2bb2fec7918bc683390b48b8
lethang301 <lethang301@gmail.com>
2016-09-23 20:43:53 +0700

    lmsup

--- eictu/app/Http/Controllers/LMSController.php
+++ eictu/app/Http/Controllers/LMSController.php
@@ -11,10 +11,14 @@ class LMSController extends Controller
 {
      public function getshow()
     {
-
-    	$datas = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->get();
-        return view('LMS.show', ['datas' => $datas]);
-
+    	$datas = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->get();    	     
+        $row1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->count();
+        $row2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('situation','>',0)->count();
+       	$s1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->sum('credit');
+       	$s2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('situation','>',0)->sum('credit');
+       	$sum1=$s1*240000;
+       	$sum2=$s2*240000;
+        return view('LMS.show', ['datas' => $datas,'row1' => $row1,'row2' => $row2,'sum1' => $sum1,'sum2' => $sum2]);
     }
 
     public function getupdate($id)
@@ -24,8 +28,7 @@ class LMSController extends Controller
     }
 
      public function update(Request $request,$id)
-    {
-    	 	
+    {   	 	
         $term = $request->input('term');          
         DB::table('schedules')->where('id',$id )->update(array('situation' =>$term));
         return redirect("LMS/show");
