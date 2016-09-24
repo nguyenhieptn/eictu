c10476cc5022124cdd48ae7a63915acb6fcf5da7
nonghinh <hinh129@gmail.com>
2016-09-24 12:56:21 +0700

    =comup

--- eictu/database/seeds/seedDormitoryTable.php
+++ eictu/database/seeds/seedDormitoryTable.php
@@ -14,12 +14,16 @@ class seedDormitoryTable extends Seeder
         /*//
         factory(App\Area::class, 10)->create();
         factory(App\Dormitory::class, 10)->create();*/
+        for($i= 1; $i <= 5; $i++)
+                DB::table('areas')->insert([
+                'name'=> 'Khu '.$i,
+            ]);
         for($i = 30; $i <= 100; $i+=3){
         	DB::table('dormitories')->insert([
         		'student_id' => $i,
         		'room' => rand(20, 100),
         		'building' => 'A'.rand(1, 11),
-        		'area_id' => rand(1, 2),
+        		'area_id' => rand(1, 5),
         		'start_on'=> '2016-0'.rand(7,9).'-01'
         		]);
         }
