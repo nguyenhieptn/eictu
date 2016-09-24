ca9df0650726bd36325d7fc5643391eb462942aa
nonghinh <hinh129@gmail.com>
2016-09-25 02:00:49 +0700

    HHdfs

--- eictu/app/Http/Controllers/DormitoryController.php
+++ eictu/app/Http/Controllers/DormitoryController.php
@@ -23,13 +23,9 @@ class DormitoryController extends Controller
                 $school = DB::table('schools')->where('id', $student->school_id)->first();
                 $dormitory = DB::table('dormitories')->where('student_id', $student->id)->first();
                 if($dormitory != ""){
-<<<<<<< HEAD
-                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', Khu 1'./*$area->name*/.', KTX '.$school->name;
-=======
                     // $area = DB::table('areas')->where('id', $dormitory->area_id)->first();
                    $area = 'Khu 1';
                 $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', '.$area/*$area->name*/.', KTX '.$school->name;
->>>>>>> d3e9b77836ee11ea816c1d3153f7356b58d7837b
 
                     $date = date('d/m/Y',strtotime($dormitory->start_on));
                    return view('dormitory.update_student', compact('str', 'date'));
@@ -102,5 +98,6 @@ class DormitoryController extends Controller
     public function logout(){
         Auth::logout();
         return redirect('/');
+        return view('schools.eICTuHomePage');
     }
 }
