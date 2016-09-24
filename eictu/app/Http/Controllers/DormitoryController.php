27dd809bbe5693370a838d0c1db7f010baf78e15
nonghinh <hinh129@gmail.com>
2016-09-25 01:37:40 +0700

    HdfgH

--- eictu/app/Http/Controllers/DormitoryController.php
+++ eictu/app/Http/Controllers/DormitoryController.php
@@ -24,8 +24,8 @@ class DormitoryController extends Controller
                 $dormitory = DB::table('dormitories')->where('student_id', $student->id)->first();
                 if($dormitory != ""){
                     // $area = DB::table('areas')->where('id', $dormitory->area_id)->first();
-                   // $area = 'Khu 1';
-                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', Khu 1'./*$area->name*/.', KTX '.$school->name;
+                   $area = 'Khu 1';
+                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', '.$area/*$area->name*/.', KTX '.$school->name;
 
                     $date = date('d/m/Y',strtotime($dormitory->start_on));
                    return view('dormitory.update_student', compact('str', 'date'));
