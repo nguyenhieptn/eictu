1b06b17a25b21f80638845a0846f35e7a494c079
bangvantien <bangtienictu2014@gmail.com>
2016-09-25 06:55:26 +0700

    Update dormitory.php

--- eictu/routes/dormitory.php
+++ eictu/routes/dormitory.php
@@ -33,7 +33,7 @@ Route::group(['prefix' => 'dormitory'], function () {
 
     Route::get('/addST', function(){
     	$st = DB::table('students')->where('code','DTC125D4802010082')->first();
-	    DB::table('dormitories')->where('id', 154)->update(['student_id'=>$st->id]);
+	    //DB::table('dormitories')->where('id', 154)->update(['student_id'=>$st->id]);
 	 return $st->id;
     });
 
