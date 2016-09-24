af68b2e8960c80d798245200db26d607981aa193
lethang301 <lethang301@gmail.com>
2016-09-24 11:36:38 +0700

    lmsss

--- eictu/app/Http/Controllers/LMSController.php
+++ eictu/app/Http/Controllers/LMSController.php
@@ -7,18 +7,36 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use DB;
 use App\Schedule;
+use Auth;
 class LMSController extends Controller
 {
      public function getshow()
     {
-    	$datas = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->get();    	     
-        $row1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->count();
-        $row2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('situation','>',0)->count();
-       	$s1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->sum('credit');
-       	$s2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('situation','>',0)->sum('credit');
-       	$sum1=$s1*240000;
-       	$sum2=$s2*240000;
-        return view('LMS.show', ['datas' => $datas,'row1' => $row1,'row2' => $row2,'sum1' => $sum1,'sum2' => $sum2]);
+        if(!Auth::guest()){
+        if(Auth::user()->type == 3)
+            $code = Auth::user()->username;
+            $student = DB::table('students')->where('code', $code)->first(); 	     
+            $row = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->count();
+            if($row==0){
+                $data = DB::table('studyprograms')->where('major_id','=',$student->major_id)->get();
+                foreach ($data as $value) {
+                    DB::table('schedules')->insert(array('student_id' => $student->id, 'studyprogram_id' => $value->id,'situation' =>0));
+                } 
+                return redirect("LMS/show");
+            }else{
+                $datas = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->get();
+                $row1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->count();
+                $row2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->where('situation','>',0)->count();
+               	$s1 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->sum('credit');
+               	$s2 = DB::table('studyprograms')->join('schedules', 'studyprograms.id', '=', 'schedules.studyprogram_id')->where('student_id','=',$student->id)->where('situation','>',0)->sum('credit');
+               	$sum1=$s1*240000;
+               	$sum2=$s2*240000;
+            return view('LMS.show', ['datas' => $datas,'row1' => $row1,'row2' => $row2,'sum1' => $sum1,'sum2' => $sum2]);
+            }
+        }
+       else{
+        return redirect('/login');
+        }
     }
 
     public function getupdate($id)
