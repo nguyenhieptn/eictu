dd1c62c716eb29e82e9f855a6e364d2c89ce34aa
Phạm Mạnh Tùng <tungfpm@gmail.com>
2016-09-24 13:52:25 +0700

    Tungfpm_Up

--- eictu/resources/views/layouts/app.blade.php
+++ eictu/resources/views/layouts/app.blade.php
@@ -31,41 +31,25 @@
     </script>
 </head>
 <body>
-{{--<nav class="navbar navbar-default navbar-static-top">--}}
-    {{--<div class="container">--}}
-        {{--<div class="navbar-header">--}}
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
 
-            {{--<!-- Collapsed Hamburger -->--}}
-            {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"--}}
-                    {{--data-target="#app-navbar-collapse">--}}
-                {{--<span class="sr-only">Toggle Navigation</span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-            {{--</button>--}}
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-            {{--<!-- Branding Image -->--}}
-            {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-                {{--{{ config('app.name', 'Laravel') }}--}}
-            {{--</a>--}}
-        {{--</div>--}}
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
 
-<<<<<<< HEAD
-        {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
-            {{--<!-- Left Side Of Navbar -->--}}
-            {{--<ul class="nav navbar-nav">--}}
-                {{--<li><a href="{{ url('schools') }}"> Schools</a></li>--}}
-                {{--<li><a href="{{ url('classes') }}"> Classes</a></li>--}}
-                {{--<li><a href="{{ url('student') }}"> Student </a></li>--}}
-                {{--<li><a href="{!! route('teacher.login') !!}"> Teacher </a></li>--}}
-                {{--<li><a href="{{ url('major') }}"> Major </a></li>--}}
-                {{--<li><a href="{{ url('dormitory') }}">Dormitory</a></li>--}}
-                {{--<li><a href="{{ url('rentHouse') }}">Rent House</a></li>--}}
-                {{--<li><a href="{{ url('iHave') }}">I Have</a></li>--}}
-                {{--<li><a href="{{ url('findjob/index') }}">Find Job</a></li>--}}
-                {{--<li><a href="{{route('iwant.search')}}">I Want</a></li>--}}
-                {{--<li><a href="{{url('iHave')}}">I Have</a></li>--}}
-=======
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
@@ -79,43 +63,42 @@
                 <li><a href="{{ url('iHave') }}">I Have</a></li>
                 <li><a href="{{ url('findjob/index') }}">Find Job</a></li>
                 <li><a href="{{route('iwant.search')}}">I Want</a></li>
-                
->>>>>>> 0d0dfb213270961329e3557b70f6bba20b33f59a
 
-            {{--</ul>--}}
 
-            {{--<!-- Right Side Of Navbar -->--}}
-            {{--<ul class="nav navbar-nav navbar-right">--}}
-                {{--<!-- Authentication Links -->--}}
-                {{--@if (Auth::guest())--}}
-                    {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
-                    {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
-                {{--@else--}}
-                    {{--<li class="dropdown">--}}
-                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
-                            {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
-                        {{--</a>--}}
+            </ul>
 
-                        {{--<ul class="dropdown-menu" role="menu">--}}
-                            {{--<li>--}}
-                                {{--<a href="{{ url('/logout') }}"--}}
-                                   {{--onclick="event.preventDefault();--}}
-                                                 {{--document.getElementById('logout-form').submit();">--}}
-                                    {{--Logout--}}
-                                {{--</a>--}}
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                                {{--<form id="logout-form" action="{{ url('/logout') }}" method="POST"--}}
-                                      {{--style="display: none;">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                {{--</form>--}}
-                            {{--</li>--}}
-                        {{--</ul>--}}
-                    {{--</li>--}}
-                {{--@endif--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</nav>--}}
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ url('/logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ url('/logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
 
 @yield('content')
 <script src="{{ url("/js/app.js") }}"></script>
