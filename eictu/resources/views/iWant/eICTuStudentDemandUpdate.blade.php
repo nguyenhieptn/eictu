af799e08a83563d6c3825a00182433195130fb0f
dvkhang <khoaimonchien@gmail.com>
2016-09-25 00:36:57 +0700

    d

--- eictu/resources/views/iWant/eICTuStudentDemandUpdate.blade.php
+++ eictu/resources/views/iWant/eICTuStudentDemandUpdate.blade.php
@@ -17,5 +17,38 @@ thoại, xin một chai nước, tìm một người đi chơi.. có thể đăn
       <hr>
     </div>
   </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-12">
+      <h4>Hiện có các lời kêu gọi/yêu cầu sau đây, mời bạn xem và trợ giúp nếu có thể:</h4>
+      <hr>
+      @if($data)
+        @foreach($data as $want)
+        <div class="col-xs-12">
+          <p style="font-weight: bold; font-size: 20px; color: black;"><span class="glyphicon glyphicon-play" style="color: #27ae60;">&nbsp;</span><a style="color: black;" href="{{route('iwant.detail', $want['id'])}}" title="">{{$want['content']}}</a></p>
+        </div>
+        @endforeach
+    @else
+      <p>Khong ai them muon gi ca luon day !</p>
+    @endif 
+
+    <div class="pagination pull-right">
+          <ul class="list-inline">
+          @if($data->currentPage() != 1)
+            <li><a href="{!! str_replace('/?','?',$data->url($data->currentPage()- 1)) !!}">Prev</a></li>
+          @endif  
+            @for($i =1; $i<= $data->lastPage(); $i++)
+            <li class="{!! ($data->currentPage() == $i) ? 'active' :'' !!}">
+              <a href="{!! str_replace('/?','?',$data->url($i)) !!}">{!!$i!!}</a>
+            </li>
+            @endfor
+          @if($data->currentPage() != $data->lastPage())  
+            <li><a href="{!! str_replace('/?','?',$data->url($data->currentPage()+ 1)) !!}">Next</a></li>
+          @endif
+          </ul>
+      </div>
+       
+    </div>
+  </div>
 </div>
 @endsection
