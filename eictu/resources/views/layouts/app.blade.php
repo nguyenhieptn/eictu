bbd277f21ee6504ecca61787d817eadf6f77bbdc
nonghinh <hinh129@gmail.com>
2016-09-24 20:12:29 +0700

    updateNav

--- eictu/resources/views/layouts/app.blade.php
+++ eictu/resources/views/layouts/app.blade.php
@@ -32,25 +32,43 @@
 </head>
 <body>
 
-{{--<nav class="navbar navbar-default navbar-static-top">--}}
-    {{--<div class="container">--}}
-        {{--<div class="navbar-header">--}}
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="{!! url('/')!!}">eICTU</a>
+    </div>
 
-            {{--<!-- Collapsed Hamburger -->--}}
-            {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"--}}
-                    {{--data-target="#app-navbar-collapse">--}}
-                {{--<span class="sr-only">Toggle Navigation</span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-            {{--</button>--}}
-
-
-
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="">@yield('title')</a></li>
+        
+      </ul>
+      <ul class="navbar-nav nav navbar-right">
+          @if(Auth::guest())
+          <li><a href="{!! url('/login')!!}" title="">Đăng nhập</a></li>
+          @else
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="{{ url('/dormitory/logout') }}">Đăng xuất</a></li>
+              </ul>
+            </li>
+          @endif
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 @yield('content')
 <script src="{{ url("/js/app.js") }}"></script>
-<!-- <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script> -->
-<!-- <script src="http://maxcdn.bootstrapcdn.com/bootstit rap/3.3.1/js/bootstrap.min.js"></scrip -->
-
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstit rap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
