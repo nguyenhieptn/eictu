fe528c654eb4004494f3f4eb2233550d6c2d58e8
dvkhang <khoaimonchien@gmail.com>
2016-09-25 03:14:51 +0700

    a

--- eictu/resources/views/iWant/eICTuStudentDemandUpdate.blade.php
+++ eictu/resources/views/iWant/eICTuStudentDemandUpdate.blade.php
@@ -2,34 +2,27 @@
 
 @section('content')
 <div class="container">
-
-<style type="text/css" media="screen">
-  .panel-default>.panel-heading{
-    background: #2ecc71;
-    color: white;
-  }
-</style>
+@if(Auth::check() && Auth::user()->type ==3)
+    <div class="row">
+    <div class="col-lg-8 col-lg-offset-2 col-xs-12">
+      <form action="{{route('iwant.status')}}" role="form" method="post" accept-charset="utf-8">
+        <input type="hidden" name="_token" value="{{Session::token()}}">
+        <div class="form-group {{$errors->has('content')? ' has-error': ''}}">
+          <textarea placeholder="Bạn muốn gì, mọi người sẽ đáp ứng !" name="content" class="form-control" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Đăng Tin</button>    
+      </form>
+      <hr>
+    </div>
+  </div>
+@endif  
   <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default ">
-                <div class="panel-heading iwant">Mong muốn của sinh viên</div>
-                <div class="panel-body">
-                 @if(Auth::check() && Auth::user()->type ==3)
-                  <form action="{{route('iwant.status')}}" role="form" method="post" accept-charset="utf-8">
-                    <input type="hidden" name="_token" value="{{Session::token()}}">
-                    <div class="form-group {{$errors->has('content')? ' has-error': ''}}">
-                      <textarea placeholder="Bạn muốn gì, mọi người sẽ đáp ứng !" name="content" class="form-control" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-success">Đăng Tin</button>    
-                  </form>
-
-                  <hr>
-                  @endif
+    <div class="col-lg-8 col-lg-offset-2 ">
       <h4>Hiện có các lời kêu gọi/yêu cầu sau đây, mời bạn xem và trợ giúp nếu có thể:</h4>
       <hr>
       @if($data)
         @foreach($data as $want)
-        <div >
+        <div class="col-xs-12">
           <p style="font-weight: bold; font-size: 20px; color: black;"><span class="glyphicon glyphicon-play" style="color: #27ae60;">&nbsp;</span><a style="color: black;" href="{{route('iwant.detail', $want['id'])}}" title="">{{$want['content']}}</a></p>
         </div>
         @endforeach
@@ -52,11 +45,8 @@
           @endif
           </ul>
       </div>
-                </div>
-            </div>
-        </div>
+       
     </div>
+  </div>
 </div>
-
-
 @endsection
