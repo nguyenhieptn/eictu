2d3b82aaa03d5aff917739f729e66397a9ced147
bangvantien <bangtienictu2014@gmail.com>
2016-09-25 01:29:51 +0700

    Update DormitoryController.php

--- eictu/app/Http/Controllers/DormitoryController.php
+++ eictu/app/Http/Controllers/DormitoryController.php
@@ -16,7 +16,28 @@ class DormitoryController extends Controller
 {
     //
     public function getUpdate(){
-       
+       if(!Auth::guest()){
+            if(Auth::user()->type == 3){
+                $code = Auth::user()->username;
+                $student = DB::table('students')->where('code', $code)->first();
+                $school = DB::table('schools')->where('id', $student->school_id)->first();
+                $dormitory = DB::table('dormitories')->where('student_id', $student->id)->first();
+                if($dormitory != ""){
+                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', Khu 1'./*$area->name*/.', KTX '.$school->name;
+
+                    $date = date('d/m/Y',strtotime($dormitory->start_on));
+                   return view('dormitory.update_student', compact('str', 'date'));
+                }
+                else
+                    return redirect()->back()->with('msg', 'Bạn không có quền cập nhật thông tin này.');
+              
+        }
+        else{
+            return redirect()->back()->with('msg', 'Bạn không có quền cập nhật thông tin này.');
+        }
+       }
+       else
+        return redirect('/login');
     }
 
     public function postUpdate(UpdateSDRequest $req){
