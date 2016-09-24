c83052cf1070e419b1857beacf18202371eacdfe
Lê Trọng Nghĩa <g.bavuongctk7@gmail.com>
2016-09-24 19:43:45 +0700

    Update detail.blade.php

--- eictu/resources/views/iHave/detail.blade.php
+++ eictu/resources/views/iHave/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-	 <div class="container find-job">
+	 <div class="container">
      <div class="row">
          <div class="col-md-12">
              <div class="panel panel-default">
