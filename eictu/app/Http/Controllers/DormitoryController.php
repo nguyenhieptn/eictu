dd4371a060a09e53869aa308c171397107d3673f
nonghinh <hinh129@gmail.com>
2016-09-25 01:20:53 +0700

    gghđ

--- eictu/app/Http/Controllers/DormitoryController.php
+++ eictu/app/Http/Controllers/DormitoryController.php
@@ -23,8 +23,9 @@ class DormitoryController extends Controller
                 $school = DB::table('schools')->where('id', $student->school_id)->first();
                 $dormitory = DB::table('dormitories')->where('student_id', $student->id)->first();
                 if($dormitory != ""){
-                    $area = DB::table('areas')->where('id', $dormitory->area_id)->first();
-                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', '.$area->name.', KTX '.$school->name;
+                    // $area = DB::table('areas')->where('id', $dormitory->area_id)->first();
+                    $area = 'Khu 1';
+                $str = "Phòng ".$dormitory->room.", Nhà ".$dormitory->building.', Khu 1'./*$area->name*/.', KTX '.$school->name;
 
                     $date = date('d/m/Y',strtotime($dormitory->start_on));
                    return view('dormitory.update_student', compact('str', 'date'));
@@ -56,7 +57,7 @@ class DormitoryController extends Controller
         $up = DB::table('dormitories')->where('student_id', $student->id)->update([
             'room' => $room,
             'building'=> $building,
-            'area_id'=> $a->id,
+            'area_id'=> 1, //$a->id,
             'start_on'=> $date2
         ]);
        return redirect()->back()->with('msg', 'Cập nhật thành công!');
