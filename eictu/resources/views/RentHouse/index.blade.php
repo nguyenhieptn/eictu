da3cf49970086770c990f699fa0f576b0c3a4b50
manhninh94 <tmninh94@gmail.com>
2016-09-24 12:20:00 +0700

    giao dien cua thay

--- eictu/resources/views/RentHouse/index.blade.php
+++ eictu/resources/views/RentHouse/index.blade.php
@@ -19,40 +19,45 @@
 
   @section('content')
   <div class="container">
+     <div style="background:#cc5200; height:40px; padding: 8px;">
+        <strong  style="color:#ffffff; font-size:20px; font-weight: 600px;">eICTuStudentRentHouseSearch - Tra cứu địa chỉ nhà trọ của Sinh viên
+            @if(!Auth::guest())
+                <a  style="color:#ffffff; font-size:20px; font-weight: 600px;"href="{!! url('dormitory/logout') !!}" title="logout" class="pull-right">Logout</a>
+            @endif
+        </strong>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <a STYLE="color:#000;" href="{{"rentHouse/create"}}"><h3 class="redirect"><i class="glyphicon glyphicon-star"></i>SINH VIÊN CẬP NHẬT NHÀ TRỌ</h3></a>
+        </div>
+    </div>
       <div class="row">
           <div class="col-md-12">
-              <div class="list-group">
-                 <a href="{{"rentHouse/create"}}" class="list-group-item active">SINH VIÊN CẬP NHẬT</a>
-               </div>
               <div class="panel panel-default">
-                  <div class="panel-heading">Tìm kiếm thông tin nhà trọ của sinh viên bằng mã số sinh viên</div>
+                  <div class="panel-heading">Đây là màn hình tìm kiếm thông tin nhà trọ của sinh viên bằng mã số sinh viên.</div>
                   <div class="panel-body">
-                      <form action = "{{url("rentHouse")}}" method = "post" class="form-horizontal">
-                        <div class="form-group">
-                          <label class="control-label col-sm-2" for="code">Mã sinh viên:</label>
-                          <div class="col-sm-10">
-                            <input type="text" class="form-control" id="code" name="code" value="">
-                          </div>
+                    <form action="{{url("rentHouse")}}" method="get" class="form-horizontal" accept-charset="utf-8">
+                        <div class="form-group" style="padding:8px;">
+                            <label class="col-sm-3 control-label" ><i class=" glyphicon glyphicon-search">&nbsp;</i>Mã số sinh viên:</label>
+                            <div class="col-sm-4">
+                                <input type="text" name="code" value="" class="form-control" placeholder="DTC...">
+                            </div>
+                            <div class="col-sm-2">
+                                <button type="submit" class="btn" style="background: #000;"><strong style="color:#ffffff">Tìm kiếm</strong></button>
+                            </div>
                         </div>
-
-                        <div class="form-group">
-                          <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default">Tìm kiếm</button>
-                          </div>
-                        </div>
-                        <input type="hidden" name="_token" value="{{csrf_token()}}"/>
-                      </form>
+                    </form>
                   </div>
               </div>
               <div class="container">
-                <h2>Kết quả tìm kiếm: <strong><?php echo $name; ?></strong></h2>
+                <strong>Kết quả tìm kiếm: </strong><strong style="color:#c7254e;"><?php echo $name; ?></strong>
                 <ul class="list-group">
                  <?php
                     $stt=1;
                     if(isset($data)){
                         foreach($data as $item){
                             $date = new DateTime($item->date_join);
-                            echo "<li class='glyphicon glyphicon-play list-group-item ' style=' color:red '>".$date->format('d/m/Y').", ".$item->hostess.", ".$item->address."</li>";
+                            echo "<li class='list-group-item ' style=' color:red '><i class='glyphicon glyphicon-triangle-right' style='color:#8c8c8c'> &nbsp;</i>".$date->format('d/m/Y').", ".$item->hostess.", ".$item->address."</li>";
                             }
                             echo "<center>".$data->appends(Request::only('code'))->links()."</center>";
                         }
