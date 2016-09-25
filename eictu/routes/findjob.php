bb401c3197c1a8a50397cf62dab6a55e4b7b4f6f
nguyenducdai <nguyenducdaiictu@gmail.com>
2016-09-25 07:37:08 +0700

    up

--- eictu/routes/findjob.php
+++ eictu/routes/findjob.php
@@ -7,16 +7,10 @@ Route::group(['prefix' => 'findjob'], function () {
         'uses' => 'FindJobController@getIndex'
     ]);
 
-    // Route::get('post', [
-    //     'as' => 'findjob.post',
-    //     'uses' => 'FindJobController@getPost'
-    // ]);
-
     Route::post('post-add', [
         'as' => 'findjob.post.add',
         'uses' => 'FindJobController@addPost'
     ]);
-    // ->middleware('auth');
 
     Route::get('detail/{id}', [
         'as' => 'findjob.detail',
