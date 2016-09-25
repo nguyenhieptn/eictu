18da74ed69350f0b886706a134bbba082d66301a
Phạm Mạnh Tùng <tungfpm@gmail.com>
2016-09-25 07:50:44 +0700

    Tungfpm_Up

--- eictu/resources/views/chat/classroom.blade.php
+++ eictu/resources/views/chat/classroom.blade.php
@@ -45,17 +45,7 @@
     ?>
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2" id="purple">
-            <h4>Phòng Chat của lớp <span><?php echo $class_room;?></span>
-                <a style="float:right;" href="{{ url('/logout') }}"
-                   onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-
-                <form id="logout-form" action="{{ url('/logout') }}" method="POST">
-                    {{ csrf_field() }}
-                </form>
-            </h4>
+            <h4>Phòng Chat của lớp <span><?php echo $class_room;?></span></h4>
         </div>
         <div class="col-sm-8 col-sm-offset-2">
             <input type="hidden" class="chat-room" value="<?php echo $class_room;?>"/>
