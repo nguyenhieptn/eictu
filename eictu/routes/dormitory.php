02dfc8274e97d62af96fd1216ba227860eda9a0a
bangvantien <bangtienictu2014@gmail.com>
2016-09-25 20:45:19 +0700

    Update dormitory.php

--- eictu/routes/dormitory.php
+++ eictu/routes/dormitory.php
@@ -9,42 +9,8 @@ Route::group(['prefix' => 'dormitory'], function () {
 	Route::post('/update', 'DormitoryController@postUpdate');
     Route::get('/search', ['as' => 'dormitory.getSearch', 'uses' => 'DormitoryController@getSearch']);
     Route::get('/query', ['as' => 'dormitory.postSearch', 'uses' => 'DormitoryController@postSearch']);
-
+	
     Route::get('/logout', ['as' => 'dormitory.postSearch', 'uses' => 'DormitoryController@logout']);
-    Route::get('/addDB', function(){
-    	 
-        for($i = 680; $i <= 750; $i+=2){
-        	DB::table('dormitories')->insert([
-        		'student_id' => $i,
-        		'room' => rand(20, 100),
-        		'building' => 'A'.rand(1, 11),
-        		'school_id' => 1,
-        		'start_on'=> '2016-0'.rand(7,9).'-01'
-        		]);
-        }
-
-        return 'ok';
-    });
-
-    Route::get('/addST', function(){
-    	$id = DB::table('students')->where('code', 'DTC125D4802010011')->first();
-    	DB::table('dormitories')->where('id', 7)->update(['student_id' =>$id->id]);
-    });  
-    Route::get('/removeAll',function(){
-    	DB::table('areas')->all()->delete();
-    	DB::table('dormitories')->all()->delete();
-
-    });
-
-    Route::get('/addST', function(){
-    	$st = DB::table('students')->where('code','DTC125D4802010082')->first();
-	    DB::table('dormitories')->where('id', 51)->update(['student_id'=>$st->id]);
-	 return $st->id;
-    });
-	Route::get('/lcd', function(){
-	    $c = DB::table('dormitories')->get();
-	 return $c;
-    });
 });
 /*=== het Quan ly ky tuc xa======*/
 
