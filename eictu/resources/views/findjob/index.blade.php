ffd7d0036f0ba116efb6d0271ec643dd2285402a
nguyenducdai <nguyenducdaiictu@gmail.com>
2016-09-30 20:30:03 +0700

    up

--- eictu/resources/views/findjob/index.blade.php
+++ eictu/resources/views/findjob/index.blade.php
@@ -7,8 +7,7 @@
             <form action="{{route('findjob.post.add')}}" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="form-group">
-                   <!-- <p class="errors"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <span class="text-danger"></span> </p> -->
-                    <textarea class="form-control ta-post" cols="30" rows="3" name="content" id="content"> </textarea>
+                    <textarea class="form-control ta-post" rows="2" name="content" id="content"> </textarea>
                     <span style="font-size:13px;color:#899090;font-style: italic;">Bạn nên nói rõ những việc bạn có thể làm, chất lượng, trình độ cũng như mức lương đòi hỏi …</span>
                     <button type="submit" class="btn btn-success pull-right btn-post"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Đăng Tin</button>
                 </div>
@@ -18,12 +17,25 @@
         <div class="panel-body panel-body-2" id="content-js">
         @foreach($datas as $data=>$item)
            <div class='media'>
-             <a href='' class='media-left' href='#'><img class='media-object' src="<?php echo ($item->avatar== null) ? "/img/user-image01.png" : $data1->avatar ?>" alt=''></a>
+             <a href='' class='media-left' href='#'><img class='media-object'  class="img-rounded" src="<?php echo ($item->avatar== null) ? "/img/user-image01.png" : $data1->avatar ?>" alt=''></a>
              <div class='media-body'> 
-             <h4 class='media-heading'>{{$item->name}}"</h4>
+               <p class="pull-right">
+               <?php 
+               if(date('d-m-Y',strtotime($item->created_at)) == date("d-m-Y")){
+                  echo "Hôm nay";
+               }elseif(date('d-m-Y',strtotime($item->created_at)) ==(date("d-m-Y")-1)){
+                  echo "Hôm qua";
+               }else{
+                  echo "date('d-m-Y',strtotime($item->created_at)";
+               }
+              ?>
+             </p>
+             <h4 class='media-heading'><strong><a href="{{route('findjob.detail',$item->sid)}}">{{$item->name}}</a></strong></h4>
              <p>{{$item->content}}</p></div>
              </div>
           @endforeach
+           <div class="pull-right">{{$datas->render()}}</div> 
         </div>
+
     </div>
 @endsection
--- eictu/resources/views/layouts/student_app.blade.php
+++ eictu/resources/views/layouts/student_app.blade.php
@@ -17,49 +17,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     @yield('css')
     <style type="text/css" media="screen">
-        /*.pagination ul li{
-            padding: 3px 10px;
-            margin: 3px;
-            font-size: 20px;
-            background: #bdc3c7;
-        }*/
-        #Left  table{
-            margin: 20px;
-        }
-
-        #Left table img{
-            width: 30px;
-            height: auto;
-            float: right;
-            margin-right: 20px;
-        }
-        #Left table #topimg {
-            width: 50px;
-            float: left;
-            margin-bottom: 20px;
-        }
-        #Left table td{
-            padding: 5px 0;
-            font-weight: bold;
-        }
-        #Left table td a{
-            text-decoration: none;
-            color: black;
-        }
-        #Left table td a:hover{
-            color:#FF2C21 ;
-        }
-        #Left table td a:checked{
-            color:#FF2C21 ;
-        }
-        #Left table td span {
-            color: #BFCDE3;
-            font-weight: bold;
-        }
-        #toptext{
-            font-weight: bold;
-            font-size: 20px;
-        }
+          img{width: }
     </style>
     <script>
         window.Laravel = <?php echo json_encode([
@@ -117,11 +75,10 @@
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
-
-    <div class="container" style="margin-top: 80px;">
-        <div class="col-md-3 col-sm-3" id="Left">
-            <div style="background-color: #ededed; position: fixed; height: 1000px;">        
-                   <div style="margin-top: 30px; margin-bottom: 30px;margin-left: 20px;">
+    <div class="container" style="margin-top:80px">
+        <div class="col-md-3 col-sm-3">
+            <div style="background-color: #ededed;">        
+                   <div>
                          <?php
                     $data1 = \App\Student::select('*')
                             ->where('code', '=', Auth::user()->username)
@@ -129,65 +86,25 @@
                     $classid = $data1 == null ? 0 : $data1->class_id;
                     $avatar = $data1!= null ? $data1->avatar==null ? "/img/avatar.jpg" : $data1->avatar."" : "/img/avatar.jpg";
                     ?>
-                       <div style="margin-top: 30px; margin-bottom: 30px;margin-left: 20px;">
-                        <img src="{{ url($avatar)}}" width="50" height="50" /> -->
-                        <span >{{$data1->name}}</span>
-                        </div>
-                       
-
+                    <div style="height:80px;line-height:80px; padding-left:10px">
+                        <img src="{{ url($avatar)}}" width="50" height="50" class="img-rounded"  />
+                        <span><strong>{{$data1->name}}</strong></span>
                     </div>
-                    <table style="text-align: left">
-
-                        @if(count($classid)>0 && $classid !=null)
-                            <tr>
-                                <td><img src="{{url('img/Gift-128.png')}}"/></td>
-                                <td>
-                                    <a href="{{route('classes.classmatersbirthday', $classid )}}">Sinh nhật bạn cùng lớp</a>
-                                </td>
-                            </tr>
-                        @endif
-
-                    <tr>
-                            <td><img src="{{url('img/newfeed.png')}}"/></td>
-                            <td><a href="{{url('student/newsfeed')}}">News Feed</a></td>
-                        </tr>
-                    <tr>
-                        <td><img src="{{url('img/Information.png')}}"/></td>
-                        <td><a href="{{route('iwant.status')}}">Tôi muốn <span>- I Want </span></a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/i_information_more_find_out_info-128.png')}}"/></td>
-                        <td><a href="{{url('/iHave')}}">Chợ đồ cũ</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/Candidate_Search-128.png')}}"/></td>
-                        <td><a href="">Tìm việc làm </a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/go-home-128.png')}}"/></td>
-                        <td><a href="{!! url('dormitory/search')!!}">Kí túc xá</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/go-home.png')}}"/></td>
-                        <td><a href="{{url('/rentHouse')}}">Nhà trọ sinh viên</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/Chat-2-128.png')}}"/></td>
-                        <td><a href="{{url('chat/classrooms')}}">Messages / CHAT</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/chat-128.png')}}"/></td>
-                        <td><a href="{{url('chat/friend')}}">Chát với bạn</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/search.png')}}"/></td>
-                        <td><a href="{{url('LMS/show')}}">Tiến độ học tập LMS</a></td>
-                    </tr>
-                    <tr>
-                        <td><img src="{{url('img/frmPhanQuyen.png')}}"/></td>
-                        <td><a href="#">Hồ sơ</a></td>
-                    </tr>
-                </table>
+                </div>
+                <div>
+                    <ul class="list-group">
+                      <li class="list-group-item"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> <a href="{{url('student/newsfeed')}}">News Feed</a></li>
+                      <li class="list-group-item"><a href="{{route('iwant.status')}}">Tôi muốn- I Want</a> </li> 
+                      <li class="list-group-item"><a href="{{url('/iHave')}}">Chợ đồ cũ</a></li> 
+                      <li class="list-group-item"><a href="{{url('/')}}"><a href="">Tìm việc làm </a></a></li> 
+                      <li class="list-group-item"><a href="{!! url('dormitory/search')!!}">Kí túc xá</a></li> 
+                      <li class="list-group-item"><a href="{{url('/rentHouse')}}">Nhà trọ sinh viên</a></li> 
+                      <li class="list-group-item"><a href="{{url('chat/classrooms')}}">Messages / CHAT</a></li>
+                      <li class="list-group-item"><a href="{{url('chat/friend')}}">Chát với bạn</a</li> 
+                      <li class="list-group-item"><a href="{{url('LMS/show')}}">Tiến độ học tập LMS</a></li> 
+                      <li class="list-group-item"><a href="#">Hồ sơ</a></li> 
+                    </ul>
+                </div>
             </div>
         </div>
         <div class="col-md-9 col-sm-9">
