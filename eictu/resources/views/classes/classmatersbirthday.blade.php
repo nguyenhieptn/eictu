4804d370400847d9eec6e97964172b5beeadf7bc
bvgiap <bvgiap@gmail.com>
2016-10-01 02:10:37 +0700

    Update classmatersbirthday.blade.php

--- eictu/resources/views/classes/classmatersbirthday.blade.php
+++ eictu/resources/views/classes/classmatersbirthday.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')		
+@extends('layouts.school_app')	
 		<link rel="stylesheet" type="text/css" 
 			href="{!! url('classes_src/css/bootstrap.min.css')!!}">
 		
@@ -11,68 +11,45 @@
 		<script src="{!! url('classes_src/js/classes.js')!!}"></script>
 
 @section('title')	
-		Trang sinh viên - Sinh nhật bạn cùng lớp 	
+		Trang sinh viên
 @endsection	
 
 
 @section('content')
-<div class="container">
-<div class="qllophoc_header">
-<!--
-	<header id="header" class="">
-		<div class="header-content">
-				<span class="title">
-					eICTuClassmatersBirthday - Sinh nhật bạn cùng lớp
-					
-					@if(!Auth::guest())
-						<span class="pull-right">
-							<img src="{!! url('classes_src/images/logout.png')!!}" />
-							<a href="{{ route('classes.logout')}}">Logout</a>
-						</span>
-					@endif
-					
-					
-				</span>
-		</div>
-	</header>
-	-->
-<!--
-	<table class="table_header" >
-		<tr>
-		  <td >eICTuClassmatersBirthday - Sinh nhật bạn cùng lớp</td>
-		  <td align="right"><img src="{!! url('quanlylophoc/images/logout.png')!!}" />Logout</td>
-		</tr>
-	</table>
--->
-</div>
-<div class="qllophoc_content">	
-<strong>30 ngày sắp tới sinh nhật của các bạn lớp mình có:</strong>
-	<div class="dssv">
-	
-	   <table class="table" >				
+<div>
+    <div class="row">
+        <div class="panel panel-default">
+			<div class="panel-heading">
+						Sinh nhật bạn cùng lớp
+			</div>
+            <div class="panel-body">
+				<strong>30 ngày sắp tới sinh nhật của các bạn lớp mình có:</strong>
+				<br><br>	
+				<table class="table" >				
 			
-			@if (count($_classmatersbirthday) <1 || $_classmatersbirthday=="")
-				<tr >
-					<th class="col-md-1" colspan="12">
-						Không có bạn nào sẽ sinh nhật trong 30 ngày tới.
-					</th>
-				</tr>
-			@else
-				@foreach ($_classmatersbirthday as $sv)						
-					<tr >
-						<td>{{ $loop->iteration }}</td>
-						<td>{{ $sv['name'] }}</td>								
-						<td>{{ $sv['gender_text'] }}</td>
-						<td>{{ $sv['birthday'] }}</td>
-						<td class="col-md-8" colspan="8">{{ $sv['deadline'] }}</th>
-					</tr>
-				@endforeach
-			@endif				
+					@if (count($_classmatersbirthday) <1 || $_classmatersbirthday=="")
+						<tr >
+							<th class="col-md-1" >
+								Không có bạn nào sẽ sinh nhật trong 30 ngày tới.
+							</th>
+						</tr>
+					@else
+						@foreach ($_classmatersbirthday as $sv)						
+							<tr >
+								<td>{{ $loop->iteration }}</td>
+								<td>{{ $sv['name'] }}</td>								
+								<td>{{ $sv['gender_text'] }}</td>
+								<td>{{ $sv['birthday'] }}</td>
+								<td>{{ $sv['deadline'] }}</th>
+							</tr>
+						@endforeach
+					@endif				
 				
-		</table>	
+				</table>	
 
-   </div>
-</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection			
 
