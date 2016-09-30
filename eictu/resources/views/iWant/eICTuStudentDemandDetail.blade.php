290e0b5115dec7699f611f0e54c4a1e1d1c72a4e
dvkhang <khoaimonchien@gmail.com>
2016-09-30 23:40:59 +0700

    Update eICTuStudentDemandDetail.blade.php

--- eictu/resources/views/iWant/eICTuStudentDemandDetail.blade.php
+++ eictu/resources/views/iWant/eICTuStudentDemandDetail.blade.php
@@ -47,26 +47,7 @@ Chi tiết lời yêu cầu
 			      	Nam
 			      @endif
 			      	,
-			      	<?php 
-
-	      		if (isset($address)) {
-	      			echo " Xóm trọ ông/bà :".$address->hostess." , ".$address->address;
-	      		}elseif (isset($address2)) {
-	      			echo  "Phòng số :".$address2->room." , Tòa nhà :".$address2->building." , Khu : 1";
-	      		}else{
-	      			echo "Không xác định được địa chỉ hiện tại của sinh viên";
-	      		}
-	      		
-	      		// if (isset($address) || isset($address2)) {
-	      		// 	if (strtotime($address->date_join) > strtotime($address2->start_on)) {
-	        //     	echo " Xóm trọ ông/bà :".$address->hostess." , ".$address->address;
-		       //    }else{
-		       //    	$area = DB::table('areas')->where('id', $address2->area_id)->first();
-		       //    	echo  "Phòng số :".$address2->room." , Tòa nhà :".$address2->building." , Khu :".$area->name;
-		       //    }
-	      			
-	      		
-	      ?>
+			      	{{$want->location}}
 
 	      <p style="font-size: 15px;">{{$want->content}}</p>
 	    	</div>
