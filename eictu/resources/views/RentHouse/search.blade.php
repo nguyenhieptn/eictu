cf5f7aac09cafc015da8ed33b5ee9523827f06ab
manhninh94 <tmninh94@gmail.com>
2016-10-01 10:22:23 +0700

    Update search.blade.php

--- eictu/resources/views/RentHouse/search.blade.php
+++ eictu/resources/views/RentHouse/search.blade.php
@@ -1,4 +1,10 @@
+@if(auth()->check())
+@if(auth()->user()->type==3)
 @extends('layouts.student_app')
+@elseif(auth()->user()->type==2)
+@extends('Teacher.master')
+@endif
+@endif
 @section('title')
 eICTuStudentRentHouseSearch - Tra cứu địa chỉ nhà trọ của Sinh viên
 @endsection
