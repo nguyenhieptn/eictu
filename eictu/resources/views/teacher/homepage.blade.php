3745a3553890b319da56b6464f35b54e25178c3d
manhninh94 <tmninh94@gmail.com>
2016-10-02 18:54:56 +0700

    Update homepage.blade.php

--- eictu/resources/views/teacher/homepage.blade.php
+++ eictu/resources/views/teacher/homepage.blade.php
@@ -55,11 +55,11 @@ function time_elapsed_string($datetime, $full = false) {
        @foreach($feed as $item)
        <div class="row boot">
        <?php 
-            $st = DB::table('students')->where('id', $item->student_id)->get();  
+            $st = DB::table('students')->where('id', $item->student_id)->first();  
             print_r($st);
            ?>  
            <div class="col-lg-2">
-                <img src="" height="100px" width="100px" />
+                <img src="{{$st->avatar}}" height="100px" width="100px" />
             <h4>
                 {{$st->name}}
            </h4>
