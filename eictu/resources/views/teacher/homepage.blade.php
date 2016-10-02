5a5a08a4b75a46800b0d2d6de401d9f1d665b87f
Lê Trọng Nghĩa <g.bavuongctk7@gmail.com>
2016-10-02 12:14:28 +0700

    Update homepage.blade.php

--- eictu/resources/views/teacher/homepage.blade.php
+++ eictu/resources/views/teacher/homepage.blade.php
@@ -59,7 +59,7 @@ function time_elapsed_string($datetime, $full = false) {
             $st = DB::table('students')->where('id', $item->student_id)->first();            
            ?>          
           <div class="col-lg-2">
-                <img src="{{$st->avatar}}" height="100px" width="100px" />
+                <img @if($st->avatar!=null) src="{{$st->avatar}}" @else src="{{url('img/avatar_null.png')}}" @endif height="100px" width="100px" />
             <h4>
                 {{$st->name}}
            </h4>
