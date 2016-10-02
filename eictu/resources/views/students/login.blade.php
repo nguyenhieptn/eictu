e84fbde3f69bbf2348abf8ae53fa6387163f684b
manhninh94 <tmninh94@gmail.com>
2016-10-02 18:52:09 +0700

    Update login.blade.php

--- eictu/resources/views/students/login.blade.php
+++ eictu/resources/views/students/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.student_app')
 
 @section('content')
-    <div class="container">
+    <div class="container" style="width:100%;">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
