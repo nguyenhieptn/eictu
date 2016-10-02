8cafd65928e871f031bc74e9c2d5ebbf810da026
bangvantien <bangtienictu2014@gmail.com>
2016-10-03 03:18:17 +0700

    Update eICTuStudentDemandDetail.blade.php

--- eictu/resources/views/iWant/eICTuStudentDemandDetail.blade.php
+++ eictu/resources/views/iWant/eICTuStudentDemandDetail.blade.php
@@ -61,12 +61,13 @@ Chi tiết lời yêu cầu
 		       {!! time_elapsed_string($want->created_at) !!}
 		   </p>
 		   <h4 class='media-heading'><strong>{!! $students->name !!}</strong></h4>
-		   <p>{{ $want->location }}</p>, 
+		  <p> 
 		   @if($student->gender == 1)
-		   Nam
+		   Nam , 
 		   @else
-		   Nữ
+		   Nữ , 
 		   @endif
+		   {{ $want->location }}</p>
 		   <a href="" title=""><p class="index-content">{{$want->content}}</p></a></div>
 		</div>
 
