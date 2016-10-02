fd3193ff950c0a238568607ae5e4bd4860cd3022
bangvantien <bangtienictu2014@gmail.com>
2016-10-02 19:26:02 +0700

    Update search.blade.php

--- eictu/resources/views/dormitory/search.blade.php
+++ eictu/resources/views/dormitory/search.blade.php
@@ -15,9 +15,11 @@ eICTuStudentDormitorySearch - Tra cứu chỗ ở trong KTX
 			@if(Session::has('msg'))
 			<div class="alert alert-warning">{!! Session::get('msg') !!}</div>
 			@endif
+			@if(!Auth()->guest())
 			<div class="col-sm-12">
 				<a href="{!! url('dormitory/update') !!}"><h3 class="redirect"><i class="fa fa-link"></i> Sinh viên cập nhật nơi ở trong KTX</h3></a>
 			</div>
+			@endif
 		</div>
 		<div class="contentUpdate">
 			<h3>Tìm kiếm thông tin nơi ở sinh viên trong KTX bằng mã sinh viên</h3>
@@ -26,7 +28,7 @@ eICTuStudentDormitorySearch - Tra cứu chỗ ở trong KTX
 					<div class="form-group">
 						<label class="col-sm-3 control-label"><i class="fa fa-circle-o"></i> Mã số sinh viên:</label>
 						<div class="col-sm-4">
-							<input type="text" name="student_id" value="" class="form-control" placeholder="DTC...">
+							<input type="text" name="student_id" value="DTC125D4802010082" class="form-control" placeholder="DTC...">
 						</div>
 						<div class="col-sm-2">
 							<button type="sumit" class="btn btn">Tìm kiếm</button>
@@ -35,8 +37,9 @@ eICTuStudentDormitorySearch - Tra cứu chỗ ở trong KTX
 				</form>
 
 				<div id="result">
-					<h3>Kết quả tìm kiếm:</h3>
+					
 					@if(isset($dormitory))
+					<h3>Kết quả tìm kiếm:</h3>
 						<div class="list">
 							<h3>
 								<i class="fa fa-caret-right"></i>
@@ -47,6 +50,14 @@ eICTuStudentDormitorySearch - Tra cứu chỗ ở trong KTX
 					@endif
 					@if(isset($none))
 						<h4 class="none">{!! $none !!}</h4>
+					@endif
+				</div>
+				<div class="historyDormitory">
+					@if(isset($student))
+					<h3>Lịch sử chuyển chỗ ở:</h3>
+						<ul>
+							{!! Cache::get($student->id) !!}
+						</ul>
 					@endif
 				</div>
 			</div>
