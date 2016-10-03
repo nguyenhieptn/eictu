b6531d3406419f5c5cc20c8f5d3ce3e3126d19ab
bangvantien <bangtienictu2014@gmail.com>
2016-10-03 16:24:40 +0700

    Update homepage.blade.php

--- eictu/resources/views/teacher/homepage.blade.php
+++ eictu/resources/views/teacher/homepage.blade.php
@@ -16,32 +16,24 @@
     @endif      	
   </div>
 <?php 
-function time_elapsed_string($datetime, $full = false) {
-    $now = new DateTime;
-    $ago = new DateTime($datetime);
-    $diff = $now->diff($ago);
-    $diff->w = floor($diff->d / 7);
-    $diff->d -= $diff->w * 7;
-    $string = array(
-        'y' => 'year',
-        'm' => 'month',
-        'w' => 'week',
-        'd' => 'day',
-        'h' => 'hour',
-        'i' => 'minute',
-        's' => 'second',
-    );
-    foreach ($string as $k => &$v) {
-        if ($diff->$k) {
-            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-        } else {
-            unset($string[$k]);
+              function time_elapsed_string($datetime, $full = false) {
+            $now = new DateTime;
+            $ago = new DateTime($datetime);
+            $diff = $now->diff($ago);
+            $diff->w = floor($diff->d / 7);
+            $diff->d -= $diff->w * 7;
+            $string = array( 'y' => 'năm', 'm' => 'tháng', 'w' => 'tuần', 'd' => 'ngày', 'h' => 'giờ', 'i' => 'phút', 's' => 'giây', );
+            foreach ($string as $k => &$v) {
+                if ($diff->$k) {
+                    $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? '' : '');
+                } else {
+                    unset($string[$k]);
+                }
+            }
+            if (!$full) $string = array_slice($string, 0, 1);
+            return $string ? implode(', ', $string) . ' trước' : 'vừa xong';
         }
-    }
-    if (!$full) $string = array_slice($string, 0, 1);
-    return $string ? implode(', ', $string) . ' ago' : 'just now';
-}
-?>
+             ?>
   <div class="row">
     <div class="col-xs-8">
       <?php 
